d188ae6eeb7cf2786e83ba49e295e2266fa2c3eb
akkigulati <41364230+akkigulati@users.noreply.github.com>
2020-08-01 13:00:31 +0530

    Update optimal-bst.java

--- Level Up DP/optimal-bst.java
+++ Level Up DP/optimal-bst.java
@@ -87,3 +87,174 @@ public class Main {
     }
 
 }
+//Memorisation
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int optimalbst(int[] keys, int[] frequency, int mem[][], int startidx, int endIdx) {
+         if(startidx==endIdx){
+             return mem[startidx][endIdx]=frequency[endIdx];
+         }
+         if(startidx<0||startidx>endIdx){
+             return 0;
+         }
+         if(mem[startidx][endIdx]!=0){
+             return mem[startidx][endIdx];
+         }
+         
+        int min=Integer.MAX_VALUE;
+        for (int cut = startidx; cut <= endIdx; cut++) {
+          //  
+            int leftTreeCost = optimalbst(keys, frequency, mem, startidx, cut - 1);
+            int rightTreeCost = optimalbst(keys, frequency, mem, cut + 1, endIdx);
+            int mycost=leftTreeCost+rightTreeCost +sumofallnodes(frequency,startidx,endIdx);
+            //adding sum of all nodes 
+            
+            if(min>mycost){
+                
+                min=mycost;
+                
+            }
+        }
+        return mem[startidx][endIdx]=min;
+    }
+    public static int sumofallnodes(int frequency[],int startidx,int endIdx){
+        int sum=0;
+        for(int i=startidx;i<=endIdx;i++){
+                sum+=frequency[i];
+            }
+            return sum;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] keys = new int[n];
+        int[] frequency = new int[n];
+        for (int i = 0; i < n; i++) {
+            keys[i] = scn.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            frequency[i] = scn.nextInt();
+        }
+        int mem[][]=new int[n][n];
+        System.out.println(optimalbst(keys, frequency, mem,0,n-1));
+    }
+
+}
+//optimized mem or recursion
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int optimalbst(int[] keys, int[] frequency, int mem[][], int startidx, int endIdx) {
+         if(startidx==endIdx){
+             return mem[startidx][endIdx]=frequency[endIdx];
+         }
+         if(startidx<0||startidx>endIdx){
+             return 0;
+         }
+         if(mem[startidx][endIdx]!=0){
+             return mem[startidx][endIdx];
+         }
+         int sum=sumofallnodes(frequency,startidx,endIdx);
+        int min=Integer.MAX_VALUE;
+        for (int cut = startidx; cut <= endIdx; cut++) {
+          //  
+            int leftTreeCost = optimalbst(keys, frequency, mem, startidx, cut - 1);
+            int rightTreeCost = optimalbst(keys, frequency, mem, cut + 1, endIdx);
+            int mycost=leftTreeCost+rightTreeCost +sum;
+            //adding sum of all nodes 
+            
+            if(min>mycost){
+                
+                min=mycost;
+                
+            }
+        }
+        return mem[startidx][endIdx]=min;
+    }
+    public static int sumofallnodes(int frequency[],int startidx,int endIdx){
+        int sum=0;
+        for(int i=startidx;i<=endIdx;i++){
+                sum+=frequency[i];
+            }
+            return sum;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] keys = new int[n];
+        int[] frequency = new int[n];
+        for (int i = 0; i < n; i++) {
+            keys[i] = scn.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            frequency[i] = scn.nextInt();
+        }
+        int mem[][]=new int[n][n];
+        System.out.println(optimalbst(keys, frequency, mem,0,n-1));
+    }
+
+}
+//more optimized 
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int optimalbst(int[] keys, int[] frequency, int mem[][], int startidx, int endIdx) {
+         if(startidx==endIdx){
+             return mem[startidx][endIdx]=frequency[endIdx];
+         }
+         if(startidx<0||startidx>endIdx){
+             return 0;
+         }
+         if(mem[startidx][endIdx]!=0){
+             return mem[startidx][endIdx];
+         }
+        //  int sum=sumofallnodes(frequency,startidx,endIdx);
+        int sum=0;
+        int min=Integer.MAX_VALUE;
+        for (int cut = startidx; cut <= endIdx; cut++) {
+            sum+=frequency[cut];
+            int leftTreeCost = optimalbst(keys, frequency, mem, startidx, cut - 1);
+            int rightTreeCost = optimalbst(keys, frequency, mem, cut + 1, endIdx);
+            int mycost=leftTreeCost+rightTreeCost;
+            //adding sum of all nodes 
+            
+            if(min>mycost){
+                
+                min=mycost;
+                
+            }
+        }
+            min+=sum;
+        return mem[startidx][endIdx]=min;
+    }
+    public static int sumofallnodes(int frequency[],int startidx,int endIdx){
+        int sum=0;
+        for(int i=startidx;i<=endIdx;i++){
+                sum+=frequency[i];
+            }
+            return sum;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] keys = new int[n];
+        int[] frequency = new int[n];
+        for (int i = 0; i < n; i++) {
+            keys[i] = scn.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            frequency[i] = scn.nextInt();
+        }
+        int mem[][]=new int[n][n];
+        System.out.println(optimalbst(keys, frequency, mem,0,n-1));
+    }
+
+}
