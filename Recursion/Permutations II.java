d9b96c66b7ecb6f50392d309b3cad5e9e5c2ebea
akkigulati <41364230+akkigulati@users.noreply.github.com>
2020-08-07 15:01:14 +0530

    Update Permutations II.java

--- Recursion/Permutations II.java
+++ Recursion/Permutations II.java
@@ -25,3 +25,32 @@ class Solution {
         }
     }
 }
+////
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums);
+        permutation(nums,new ArrayList<>(),new boolean[nums.length],ans);
+        return ans;
+    }
+    public static void permutation(int[] nums,List<Integer> ai,boolean vis[],List<List<Integer>> ans){
+        if(ai.size()==nums.length){
+            ans.add(new ArrayList(ai));
+            return;
+        }
+        // HashSet<Integer> unique=new HashSet<>();
+        int prev=(int)-1e8;
+        for(int i=0;i<nums.length;i++){
+            
+            if(!vis[i]&&prev!=nums[i]){
+                ai.add(nums[i]);
+                vis[i]=true;
+                permutation(nums,ai,vis,ans);   
+                ai.remove(ai.size()-1);
+                vis[i]=false;
+                prev=nums[i];
+                // unique.add(nums[i]);
+            }              
+        }
+    }
+}
