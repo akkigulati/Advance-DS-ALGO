0ec1006d83b04f369f5f2a908211d3b26890e51c
akkigulati <41364230+akkigulati@users.noreply.github.com>
2020-08-14 14:57:36 +0530

    leet 260

---
+++ Bit Manipulation/SingleNumberIII.java
@@ -0,0 +1,22 @@
+//
+class Solution {	
+	public int[] singleNumber(int[] nums) {
+        int xor=0;
+        for(int n:nums){
+            xor^=n;
+        }
+        //mask is last set bit..
+        int mask=(xor&(-xor));
+        int a=0,b=0;
+        for(int n:nums){
+            if((n&mask)!=0){
+                a^=n;
+            }else{
+                b^=n;
+            }
+        }
+        int ans[]={
+            a,b
+        };
+        return ans;}
+}
