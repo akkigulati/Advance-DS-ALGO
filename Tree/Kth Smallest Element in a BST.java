4ab2ed1f4e4572ec4204db5e7146f5fa3238df4a
akkigulati <41364230+akkigulati@users.noreply.github.com>
2020-08-23 15:15:29 +0530

    230 Leetcode Bst

--- Tree/Kth Smallest Element in a BST.java
+++ Tree/Kth Smallest Element in a BST.java
@@ -37,3 +37,38 @@ class Solution {
         return KthSmallestAns;
     }
 }
+/////////////////////////////////////////////////////////////////////////////
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void pushAllNext(Stack<TreeNode> st,TreeNode node){
+        while(node!=null){
+            st.push(node);
+            node = node.left;
+        }
+    }
+
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> st = new Stack<>();
+        pushAllNext(st,root);
+
+        while(--k != 0){
+            TreeNode rNode = st.pop();
+            pushAllNext(st,rNode.right);
+        }
+        return st.peek().val;
+    }
+}
